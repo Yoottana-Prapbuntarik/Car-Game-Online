8cbb630cac29b74943eb907acba4e9a9ea033c90
nap3831 <nap3831@gmail.com>
2020-01-28 12:06:00 +0700

    initialized project unity car game online

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿//adapted from example script available at
+//https://docs.unity3d.com/ScriptReference/Input.GetAxis.html
+using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+    Rigidbody rb;
+	public float speed = 10.0F;
+    public float rotationSpeed = 100.0F;
+
+
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+    }
+	// Update is called once per frame
+	void Update () {
+	
+        float translation = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+        //หาว่าเมื่อกดให้หมุนในแกน Y เท่านั้น 
+        Quaternion turn = Quaternion.Euler(0f, rotation, 0f);
+        rb.MovePosition(rb.position + this.transform.forward * translation);
+        rb.MoveRotation(rb.rotation * turn);
+        /*
+        transform.Translate(0, 0, translation);
+        transform.Rotate(0, rotation, 0);
+	    */
+    }
+}
---
+++ Assets/Scripts/SetupLocalPlayer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class SetupLocalPlayer : NetworkBehaviour
+{
+    public Text NamePrefab;
+    private Text NameLabel;
+    public Transform namePosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (isLocalPlayer)
+        {
+            GetComponent<PlayerController>().enabled = true;
+            CameraFollow360.player = this.gameObject.transform;
+        }
+        else
+        {
+            GetComponent<PlayerController>().enabled = false;
+        }
+        //finding where is canvas
+        GameObject canvas = GameObject.FindWithTag("TextCanvas");
+        NameLabel = Instantiate(NamePrefab, Vector3.zero, Quaternion.identity) as Text;
+        //transform follow canvas 
+        NameLabel.transform.SetParent(canvas.transform);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
