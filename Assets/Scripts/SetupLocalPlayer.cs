9c6c173328e7c861a729980f7bbc09ebf45bb2c5
nap3831 <nap3831@gmail.com>
2020-01-29 11:47:28 +0700

    create function change name and color

--- Assets/Scripts/SetupLocalPlayer.cs
+++ Assets/Scripts/SetupLocalPlayer.cs
@@ -9,72 +9,16 @@ public class SetupLocalPlayer : NetworkBehaviour
     public Text NamePrefab;
     private Text NameLabel;
     public Transform namePosition;
-    string textboxName = "";
-    string color="";
-    [SyncVar (hook = "OnChangeName")]
     public string pName = "Player";
-    [SyncVar(hook = "OnChangeColor")]
-    public string pColor = "#ffffff";
-    void OnChangeName(string n)
-    {
-        pName = n;
-        NameLabel.text = pName;
-    }
-    void OnChangeColor(string n)
-    {
-        pColor = n;
-        Renderer[] rends = GetComponentsInChildren<Renderer>();
-
-        foreach (Renderer r in rends)
-        {
-            if (r.gameObject.name == "BODY")
-            {
-                r.material.SetColor("_Color", ColorFromHex(pColor));
-            }
-        }
-    }
-    Color ColorFromHex(string hex)
-    {
-        hex = hex.Replace("0x", "");
-        hex = hex.Replace("#", "");
-        byte a = 255;
-        byte r = byte.Parse(hex.Substring(0, 2), System.Globalization.NumberStyles.HexNumber);
-        byte g = byte.Parse(hex.Substring(2, 2), System.Globalization.NumberStyles.HexNumber);
-        byte b = byte.Parse(hex.Substring(4, 2), System.Globalization.NumberStyles.HexNumber);
-        if (hex.Length == 8)
-        {
-            a = byte.Parse(hex.Substring(4, 2), System.Globalization.NumberStyles.HexNumber);
-        }
-        return new Color32(r, g, b, a);
-    }
     private void OnGUI()
     {
         if (isLocalPlayer)
         {
-            textboxName = GUI.TextField(new Rect(25, 15, 100, 25), textboxName);
-            if (GUI.Button(new Rect(130, 15, 35, 35), "Set"))
-            {
-                CmdChangeName(textboxName);
-            }
-            color = GUI.TextField(new Rect(200, 50, 100, 25), color);
-            if (GUI.Button(new Rect(300, 50, 35, 35), "Set"))
-            {
-                CmdOnChangeColor(color);
-            }
+            pName = GUI.TextField(new Rect(25, 15, 100, 25), pName);
+            NameLabel.text = pName;
         }
     }
     // Start is called before the first frame update
-    [Command]
-    public void CmdChangeName(string name)
-    {
-        pName = name;
-        NameLabel.text = pName;
-    }
-    [Command]
-    public void CmdOnChangeColor(string newcolor)
-    {
-        pColor = newcolor;
-    }
     void Start()
     {
         if (isLocalPlayer)
@@ -99,6 +43,6 @@ public class SetupLocalPlayer : NetworkBehaviour
         Vector3 nameLabelPos =
             Camera.main.WorldToScreenPoint(namePosition.position);
         NameLabel.transform.position = nameLabelPos;
-
+        
     }
 }
